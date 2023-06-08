62835316fcb0eeb403c9f160dae5da1899d851c8
Anna Cole <acrrj123@gmail.com>
2023-06-08 15:51:48 -0500

    missing only unlike function

--- js/index.js
+++ js/index.js
@@ -6,50 +6,62 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function renderTitles(book) {
     //console.log(book.title)
-    const list = document.getElementById('list')
-    const li = document.createElement('li')
-    li.textContent = book.title
-    list.append(li)
-    li.addEventListener('click', () => renderBookInfo(book))
+    const listContainer = document.getElementById('list')
+    const title = document.createElement('li')
+    title.textContent = book.title
+    listContainer.append(title)
+    title.addEventListener('click', () => renderDetails(book))
   }
 
-  function renderBookInfo(book) {
+  function renderDetails(book) {
     //console.log(book)
-    const div = document.getElementById('show-panel')
-    //console.log(div)
-    div.innerHTML = ''
+    const panel = document.getElementById('show-panel')
+    //console.log(panel)
+    panel.innerHTML = ''
     const img = document.createElement('img')
     img.src = `${book.img_url}`
-    const title = document.createElement('h4')
-    title.textContent = `${book.title}`
+    const titlePanel = document.createElement('h4')
+    titlePanel.textContent = `${book.title}`
     const subtitle = document.createElement('h4')
     subtitle.textContent = `${book.subtitle}`
     const author = document.createElement('h4')
     author.textContent = `${book.author}`
-    const p = document.createElement('p')
-    p.textContent = `${book.description}`
-    div.append(img, title, subtitle, author, p)
-    const ul = document.createElement('ul')
-    ul.id = 'list-of-users'
-    const users = book.users
-    users.forEach(user => {
+    const description = document.createElement('p')
+    description.textContent = `${book.description}`
+    panel.append(img, titlePanel, subtitle, author, description)
+    const listOfUsers = document.createElement('ul')
+    //console.log(book.users)
+    book.users.forEach(user => {
       const liUser = document.createElement('li')
       liUser.textContent = user.username
-      liUser.id = user.id
-      //liUser.name = user.username
-      ul.appendChild(liUser)
-      div.appendChild(ul)
+      //console.log(user.username)
+      //liUser.id = user.id
+      listOfUsers.appendChild(liUser)
+      panel.appendChild(listOfUsers)
     })
-    const button = document.createElement('button')
-    button.textContent = 'Like'
-    button.id = `${book.id}`
-    div.appendChild(button)
-    button.addEventListener('click', () => likeBook(book))
+    const likeBtn = document.createElement('button')
+    likeBtn.textContent = 'Like'
+    //likeBtn.id = `${book.id}`
+    panel.appendChild(likeBtn)
+    likeBtn.addEventListener('click', () => likeBook(book, book.users))
   }
 
-  function likeBook(book) {
-    console.log(book.users)
+  function likeBook(book, usersArr) {
+    //console.log(book.id, usersArr)
+    usersArr.push({"id": 1, "username": "pouros"})
+
+    fetch(`http://localhost:3000/books/${book.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({users: usersArr})
+    })
+    .then(resp => resp.json())
+    .then(updatedBook => { 
+      renderDetails(updatedBook) 
+      console.log(updatedBook) 
+    })
   }
-
-
-});
+})
