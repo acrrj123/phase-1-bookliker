970d3ce69f116ecca87c98debfc49b039aae13f7
Anna Cole <acrrj123@gmail.com>
2023-06-09 15:56:08 -0500

    Lab done

--- js/index.js
+++ js/index.js
@@ -44,28 +44,71 @@ document.addEventListener("DOMContentLoaded", function() {
     likeBtn.textContent = 'Like'
     //likeBtn.id = `${book.id}`
     panel.appendChild(likeBtn)
-    likeBtn.addEventListener('click', () => likeBook(book, book.users))
+    likeBtn.addEventListener('click', () => handleLikeClick(book, book.users))
   }
 
-  function likeBook(book, usersArr) {
+  function handleLikeClick(book, usersArr) {
     //console.log(book.id, usersArr)
-    usersArr.push({"id": 1, "username": "pouros"})
 
-    fetch(`http://localhost:3000/books/${book.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      },
-      body: JSON.stringify({users: usersArr})
+    if(likeBtn.textContent === 'Like') {
+      usersArr.push({"id": 1, "username": "pouros"})
+      fetch(`http://localhost:3000/books/${book.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({users: usersArr})
+      })
+      .then(resp => resp.json())
+      .then(book => renderUnlike(book))
+    }
+
+    else if(likeBtn.textContent === 'Unlike') {
+      usersArr.pop()
+      fetch(`http://localhost:3000/books/${book.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({users: usersArr})
+      })
+      .then(resp => resp.json())
+      .then(book => renderDetails(book))
+    }
+  }
+
+  function renderUnlike(book) {
+    //console.log(book)
+    const panel = document.getElementById('show-panel')
+    //console.log(panel)
+    panel.innerHTML = ''
+    const img = document.createElement('img')
+    img.src = `${book.img_url}`
+    const titlePanel = document.createElement('h4')
+    titlePanel.textContent = `${book.title}`
+    const subtitle = document.createElement('h4')
+    subtitle.textContent = `${book.subtitle}`
+    const author = document.createElement('h4')
+    author.textContent = `${book.author}`
+    const description = document.createElement('p')
+    description.textContent = `${book.description}`
+    panel.append(img, titlePanel, subtitle, author, description)
+    const listOfUsers = document.createElement('ul')
+    //console.log(book.users)
+    book.users.forEach(user => {
+      const liUser = document.createElement('li')
+      liUser.textContent = user.username
+      //console.log(user.username)
+      //liUser.id = user.id
+      listOfUsers.appendChild(liUser)
+      panel.appendChild(listOfUsers)
     })
-    .then(resp => resp.json())
-    .then(data => {
-      if (likeBtn.textContent === 'Like') {
-        renderDetails(data)
-        console.log(data)
-        likeBtn.textContent = 'Unlike'
-      }
-    })
+    likeBtn.textContent = 'Unlike'
+    //likeBtn.id = `${book.id}`
+    panel.appendChild(likeBtn)
+    likeBtn.addEventListener('click', () => handleLikeClick(book, book.users))
   }
 })
+  
